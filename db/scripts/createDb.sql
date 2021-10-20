90e00d58805cf58193f1f219adae4307c2f716a8
Julien COMOLLI <comoju14041@iutparis.local>
2021-10-20 14:10:28 +0200

    Update: Fix and improve database structure

--- db/scripts/createDb.sql
+++ db/scripts/createDb.sql
@@ -1,20 +1,20 @@
 CREATE TABLE Users (
-    Id INT NOT NULL,
-    Username VARCHAR(20) NOT NULL,
-    Email VARCHAR(320) NOT NULL,
-    UserDesc VARCHAR(280),
-    Pass VARCHAR(80) NOT NULL,
-    PicturePath VARCHAR(80),
-    AccountState INTEGER NOT NULL,
-    CONSTRAINT PK_Users PRIMARY KEY (Id),
-    CONSTRAINT U_Users_Username UNIQUE (Username),
-    CONSTRAINT U_Users_Email UNIQUE (Email)
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username VARCHAR(20) NOT NULL,
+    email VARCHAR(320) NOT NULL,
+    user_desc VARCHAR(280),
+    pass VARCHAR(80) NOT NULL,
+    picture_path VARCHAR(80),
+    creation_date DATE NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    last_connection DATE,
+    account_state INTEGER NOT NULL DEFAULT 0,
+    CONSTRAINT u_users_username UNIQUE (username),
+    CONSTRAINT u_users_email UNIQUE (email)
 );
 
 CREATE TABLE Games (
-    Id INT NOT NULL,
-    GameName VARCHAR(30) NOT NULL,
-    GameDesc VARCHAR(280),
-    CONSTRAINT PK_Games PRIMARY KEY (Id),
-    CONSTRAINT U_Games_GameName UNIQUE (GameName)
+    id INTEGER PRIMARY KEY,
+    game_name VARCHAR(30) NOT NULL,
+    game_desc VARCHAR(280),
+    CONSTRAINT u_games_gamename UNIQUE (game_name)
 );
--- db/scripts/populateDb.sql
+++ db/scripts/populateDb.sql
@@ -1,4 +1,5 @@
-INSERT INTO Users (Id, Username, Email, UserDesc, Pass, AccountState) VALUES (1, 'Francisco', 'francisco@gmail.com', 'J''aime les poissons.', 'PASSWORD', 0);
-INSERT INTO Users (Id, Username, Email, UserDesc, Pass, AccountState) VALUES (2, 'Marie', 'marie@gmail.com', 'Vive la salade !!', 'PASSWORD', 1);
-
-INSERT INTO Games (Id, GameName, GameDesc) VALUES (1, 'Pendu', 'M''enfin tout le monde connaît le pendu !');
+INSERT INTO Users (username, email, user_desc, pass) VALUES ('Marie', 'marie@gmail.com', 'Vive la salade !!', 'PASSWORD');
+INSERT INTO Users (username, email, user_desc, pass) VALUES ('Francisco', 'francisco@gmail.com', 'J''adore le cinéma.', 'PASSWORD');
+INSERT INTO Users (username, email, user_desc, pass) VALUES ('piwi', 'piwi@gmail.com', 'Miaou !!!!!', 'PASSWORD');
+INSERT INTO Games (game_name, game_desc) VALUES ('Pendu', 'M''enfin tout le monde connaît le pendu !');
+INSERT INTO Games (game_name, game_desc) VALUES ('Quizz', 'On pose des questions vous répondez !');
