0f6fdf1d71116f7b010be07880779a13e9a60e3f
Sasha Contrepois <sasha.contrepois@hotmail.fr>
2021-10-20 12:00:58 +0200

    refactor : Change integrity constraints declarations

--- db/scripts/createDb.sql
+++ db/scripts/createDb.sql
@@ -1,18 +1,20 @@
 CREATE TABLE Users (
-    Id INT PRIMARY KEY NOT NULL,
+    Id INT NOT NULL,
     Username VARCHAR(20) NOT NULL,
     Email VARCHAR(320) NOT NULL,
     UserDesc VARCHAR(280),
     Pass VARCHAR(80) NOT NULL,
     PicturePath VARCHAR(80),
     AccountState INTEGER NOT NULL,
-    UNIQUE (Username),
-    UNIQUE (Email)
+    CONSTRAINT PK_Users PRIMARY KEY (Id),
+    CONSTRAINT U_Users_Username UNIQUE (Username),
+    CONSTRAINT U_Users_Email UNIQUE (Email)
 );
 
 CREATE TABLE Games (
-    Id INT PRIMARY KEY NOT NULL,
+    Id INT NOT NULL,
     GameName VARCHAR(30) NOT NULL,
     GameDesc VARCHAR(280),
-    UNIQUE (GameName)
+    CONSTRAINT PK_Games PRIMARY KEY (Id),
+    CONSTRAINT U_Games_GameName UNIQUE (GameName)
 );
