3e17e59e95cc73a083ed76125035186267b882a4
JulienComolli <comolli.julien@gmail.com>
2021-11-02 00:03:01 +0100

    Update: DataBase connection handled in a module

---
+++ modules/dbConn.js
@@ -0,0 +1,18 @@
+import Database from "better-sqlite3";
+import fs from "fs";
+
+import { DBNAME } from "../config/public-config.js";
+
+var DB;
+
+// Init DB
+if(!fs.existsSync(`./db/${DBNAME}`)){
+    console.log('Setting up database structure ...');
+    DB = new Database(`./db/${DBNAME}`);
+    DB.exec(fs.readFileSync('./db/scripts/createDb.sql', 'utf-8'));
+    DB.exec(fs.readFileSync('./db/scripts/populateDb.sql', 'utf-8'));
+} else {
+    DB = new Database(`./db/${DBNAME}`); // Charger en mémoire
+}
+
+export default DB;
